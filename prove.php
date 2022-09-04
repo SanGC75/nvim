c3b509a8fdb618b56705e4a03c2505ad5b4e53f6
SanGC75 <sanguti75@gmail.com>
2022-09-04 19:25:58 -0400

    snip

--- prove.php
+++ prove.php
@@ -10,4 +10,14 @@
 
 	</body>
 </html>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>HOLA MUNDO</title>
+	</head>
+	<body>
 
+	</body>
+</html>
